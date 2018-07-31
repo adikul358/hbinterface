99dc42a5cefb4c8f88a430cda98947ed9ec0ec52
adikul358 <adi.kul358@gmail.com>
2018-07-31 07:42:06 +0530

    update

--- book.php
+++ book.php
@@ -13,19 +13,19 @@
 		$m++; 
 		$currdate = date("Y\-m\-d", mktime(0,0,0,$m, $_GET['d'], $_GET['y']));
 
-		$booking_query = "SELECT start, name FROM bookings WHERE date='$currdate'";
+		$booking_query = "SELECT * FROM bookings WHERE date='$currdate'  ORDER BY start";
 		$result = mysqli_query($link, $booking_query);
 
 		if (!$result) {
 			exit("Failed to fetch:<br>" . mysqli_error($link));
 		}
-
+		
 		$bookings = array();
 		while($row = mysqli_fetch_assoc($result)) {
 			$bookings[] = $row;
 		}
 		$r = mysqli_num_rows($result);
-
+		
 		if (isset($_POST['submit'])) {
 			$name = mysqli_real_escape_string($link, $_POST['name']);
 			$email = mysqli_real_escape_string($link, $_POST['email']);
@@ -33,8 +33,16 @@
 			$event = mysqli_real_escape_string($link, $_POST['event']);
 			$start = mysqli_real_escape_string($link, $_POST['start']);
 			$end = mysqli_real_escape_string($link, $_POST['end']);
+			
+			$event_query = "INSERT INTO bookings (event, date, start, end, name, phone, email)
+			VALUES ('$event', '$currdate', '$start', '$end', '$name', '$phone', '$email')";
+			$eresult = mysqli_query($link, $event_query);
 
-			// $event_query = "INSERT INTO "
+			if(!$eresult) {
+				exit("Failed to fetch:<br>" . mysqli_error($link));
+			}
+
+
 		}		
 	?>
 </head>
@@ -52,6 +60,7 @@
 	</div>
 
 	<body>
+		<div id="bofoma">
 		<div id="content">
 			<div id="titlespan" style="display: block; border-bottom: 2px solid black; width: 80%;">Booked Slots</div>
 			<div id="bkdslts">
@@ -71,17 +80,16 @@
 								<th class="daysheader">Event</th>
 								<th class="daysheader">Start time</th>
 								<th class="daysheader">End time</th>
-								<th class="daysheader" colspan="2">Booker</th>
+								<th class="daysheader">Person Booking</th>
 							</tr>';
 	
 							foreach ($bookings as $row) {
 								$html = '<tr>';
-								$html .= '<td class="bkdslts">' . $counter . '</td>';
-								$html .= '<td class="bkdslts">' . $event . '</td>';
-								$html .= '<td class="bkdslts">' . $row['start'] . '</td>';
-								$html .= '<td class="bkdslts">' . $row['end'] . '</td>';
-								$html .= '<td class="bkdslts">' . $row['name'] . '</td>';
-								$html .= '<td class="bkdslts">' . '<a onclick="discon">Contact Info</a>' . '</td>';
+								$html .= '<td class="bkdslts" style="width:80px">' . $counter . '</td>';
+								$html .= '<td class="bkdslts" style="text-align:left; width:400px">' . $row['event'] . '</td>';
+								$html .= '<td class="bkdslts" style="width:100px">' . $row['start'] . '</td>';
+								$html .= '<td class="bkdslts" style="width:100px">' . $row['end'] . '</td>';
+								$html .= '<td class="bkdslts" style="width:150px">' . $row['name'] . '<h5><a id="contact" onclick="coninfo()" href="#">Contact Info</a></h5></td>';
 								$html .= '</tr>';
 								$counter++;
 								echo $html;
@@ -91,13 +99,16 @@
 					?>
 				</table>
 			</div>
-			<div style="margin:15px">
+		</div>
+		<div id="add">
+			<div id=k style="margin:15px">
 				<span id="titlespan" style="margin-right: 15px">Book a Slot</span>
 				<a href="#" id="add" onclick="launchForm()">
 					<img src="images/add.svg" id="addico">
 				</a>
 			</div>
 		</div>
+					</div>
 		<div id="booking-form">
 			<div id=mbf>
 				<form method="POST">
@@ -105,7 +116,7 @@
 
 						<label>Event Name</label>
 						<br>
-						<input type=text name="event">
+						<input type=text name="event" require>
 						<br>
 						<br>
 
@@ -133,33 +144,51 @@
 							<div>
 								<label>End Time</label>
 								<br>
-								<select name="end" id=e></select>
+								<select name="end" id=e>
+								<?php
+
+									$slots = array();
+									for($i = strtotime("09:35"); $i<= strtotime("15:25"); $i= $i+35*60) {
+										$slots[] = date("h:i", $i); 
+										$server_slots[] = date("H:i:s", $i); 
+									}
+									$c = 0;
+									foreach ($slots as $s) {
+										echo '<option value="' . $server_slots[$c] . '" name="' .$s .'">' . $s . '</option>';
+										$c++;
+									}
+									?>
+								</select>
 								<br>
-						</div>
-						<br>
-						<br>
-
-						<label>Name of Booker</label>
-						<br>
-						<input type="text" name="name">
-						<br>
-						<br>
-						<label>Contact No.</label>
-						<br>
-						<input type="number" name="phone">
-						<br>
-						<br>
-						<label>Email</label>
-						<br>
-						<input type="email" name="email">
-						<br>
-						<br>
-
-
-					</div>
-					<input type="submit" value="Book Hall" onclick="removeForm()" style="float:right">
-				</form>
 							</div>
+							<br>
+							<br>
+
+							<label>Name of Person Booking</label>
+							<br>
+							<input type="text" name="name"  require>
+							<br>
+							<br>
+							<label>Contact No.</label>
+							<br>
+							<input type="number" name="phone"  require>
+							<br>
+							<br>
+							<label>Email</label>
+							<br>
+							<input type="email" name="email"  require>
+							<br>
+							<br>
+
+
+						</div>
+					</div>
+					<br>
+					<div>
+						<input type="submit" value="Book Hall" onclick="removeForm()" style="float:right">
+					</div>
+				</form>
+			</div>
 	</body>
 
 </html>
