25387904881fa965d6624e5ffecb035382aef777
adikul358 <adi.kul358@gmail.com>
2018-08-17 21:02:35 +0530

    mdbootstrap integration

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
 	<link href="style.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="Java-Calendar/main.css">
 	<title>WCH Booking Interface - Shiv Nadar School, Noida</title>
+	<!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
