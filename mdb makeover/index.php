8930ecb6608fab65c25794c338342f5012fbc67a
adikul358 <adi.kul358@gmail.com>
2018-08-19 16:22:00 +0530

    calendar integration

--- mdb makeover/index.php
+++ mdb makeover/index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>WCH Booking Interface - Shiv Nadar School, Noida</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
-    <!-- Your custom styles (optional) -->
-    <link href="css/style.css" rel="stylesheet">
+    
+    <head>
+        <title>WCH Booking Interface - Shiv Nadar School, Noida</title>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="css/mdb.min.css" rel="stylesheet">
+        <!-- Your custom styles (optional) -->
+        <link href="css/style.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Economica' rel='stylesheet' type='text/css'>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+        <!-- Respomsive slider -->
+        <link href="calendar 0.9/css/responsive-calendar.css" rel="stylesheet">
 </head>
 
 <body>
@@ -94,7 +99,47 @@
 
             </div>
         </div>
-        <div id="divcalendartable"></div>
+    <div class="container">
+      <!-- Responsive calendar - START -->
+    	<div class="responsive-calendar">
+        <div class="controls">
+            <a class="pull-left" data-go="prev"><div class="btn btn-primary">Prev</div></a>
+            <h4><span data-head-year></span> <span data-head-month></span></h4>
+            <a class="pull-right" data-go="next"><div class="btn btn-primary">Next</div></a>
+        </div><hr/>
+        <div class="day-headers">
+          <div class="day header">Mon</div>
+          <div class="day header">Tue</div>
+          <div class="day header">Wed</div>
+          <div class="day header">Thu</div>
+          <div class="day header">Fri</div>
+          <div class="day header">Sat</div>
+          <div class="day header">Sun</div>
+        </div>
+        <div class="days" data-group="days">
+          
+        </div>
+      </div>
+      <!-- Responsive calendar - END -->
+    </div>
+    <script src="calendar 0.9/js/jquery.js"></script>
+    <script src="calendar 0.9/js/bootstrap.min.js"></script>
+    <script src="calendar 0.9/js/responsive-calendar.js"></script>
+    <script type="text/javascript">
+
+
+      $(document).ready(function () {
+        $(".responsive-calendar").responsiveCalendar({
+          time: "2018-08",
+          events: {
+            "2013-04-30": {"number": 5, "url": "http://w3widgets.com/responsive-slider"},
+            "2013-04-26": {"number": 1, "url": "http://w3widgets.com"}, 
+            "2013-05-03":{"number": 1}, 
+            "2013-06-12": {}}
+        });
+      });
+    </script>
+        </div>
     </div>
 
 
