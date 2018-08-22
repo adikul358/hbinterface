325e85c2bff261caa7421b0762edeeccdb09ecb0
adikul358 <adi.kul358@gmail.com>
2018-08-22 15:13:19 +0530

    book page

--- mdb-makeover/php/functions.php
+++ mdb-makeover/php/functions.php
@@ -82,4 +82,40 @@
         }
     
     }
+    
+    function time_slots_display() {
+        global $bookings;
+        $counter = 1;
+        $html = "";
+        $slts = array("9:00 AM - 10:10 AM",
+                    "10:10 AM - 11:20 AM",
+                    "11:20 AM - 12:30 PM",
+                    "12:30 PM - 1:40 PM",
+                    "2:10 PM - 3:20 PM");
+        $used_slts = array();
+        $avail_slts = array();
+        $ava_slts = array();
+        
+        foreach ($bookings as $slt) {
+            $used_slts[] = ($slt['slot_no'] >  0) ? $slts[$slt['slot_no']-1] : "";
+        }
+        $ava_slts = array_diff($slts, $used_slts);
+        
+        foreach ($ava_slts as $v)  {
+            $avail_slts[] = $v;
+        }
+        
+        foreach ($avail_slts as $slt) {
+            $html = '<div class="custom-control custom-checkbox" style=margin-bottom:5px>
+            <input type="checkbox" class="custom-control-input" id=timeslot';
+            $html .= $counter;
+            $html .= '><label class="custom-control-label" for=timeslot';
+            $html .= $counter;
+            $html .= '>';
+            $html .= $slt;
+            $html .= '</label></div>';
+            echo $html;
+            $counter++;
+        }
+    }
 ?>
