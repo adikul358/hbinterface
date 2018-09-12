3ace6d6568363c6ed39176600175c8cb0f192f2b
adikul358 <38763258+adikul358@users.noreply.github.com>
2018-09-13 03:33:31 +0530

    Silly mistake

--- php/ajax/get_last_slot.php
+++ php/ajax/get_last_slot.php
@@ -1,5 +1,5 @@
 <?php
-    require 'functions.php';
+    require '../functions.php';
     $check = time_slots_display(date("Y-m-d", strtotime("today")), false);
     if ($check) {
         echo "true";
