138e38855d37285d024e7ac0da7344380a68a137
adikul358 <adi.kul358@gmail.com>
2018-09-13 23:00:36 +0530

    test 3

--- php/functions.php
+++ php/functions.php
@@ -6,14 +6,21 @@
     // fetch color array from strangeplanet.fr
     function set_colors($steps) {
         $url="https://www.strangeplanet.fr/work/gradient-generator/?c=" . $steps . ":007E33:ff8800:C62828";
-        libxml_use_internal_errors(true);
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        // $html = curl_exec($ch);
+        // curl_close($ch);
+        // echo "<br>";
         print_r($url);
         echo "<br>";
+        libxml_use_internal_errors(true);
         $doc = new DOMDocument;
         $doc->loadHTMLFile($url);
         $xpath = new DOMXpath($doc);
         $node = $xpath->query('//textarea[@cols="80"]')->item(0);
-        print_r($node);
+        print_r("node: " + $node);
         echo "<br>";
         
         $textContent = $node->textContent;
