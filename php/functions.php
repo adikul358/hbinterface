ebebe16aa9f2e1ac75cc18f6a77db88b0570b319
adikul358 <adi.kul358@gmail.com>
2018-09-13 22:38:15 +0530

    test

--- php/functions.php
+++ php/functions.php
@@ -6,22 +6,35 @@
     // fetch color array from strangeplanet.fr
     function set_colors($steps) {
         $url="https://www.strangeplanet.fr/work/gradient-generator/?c=" . $steps . ":007E33:ff8800:C62828";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         // curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-        $html = curl_exec($ch);
-        curl_close($ch);
-        libxml_use_internal_errors(true);
+        // $html = curl_exec($ch);
+        // curl_close($ch);
+        // libxml_use_internal_errors(true);
+        echo "<br>";
+        print_r($url);
+        echo "<br>";
         $doc = new DOMDocument;
-        $doc->loadHTML($html);
+        $doc->loadHTML($url);
         $xpath = new DOMXpath($doc);
         $node = $xpath->query('//textarea[@cols="80"]')->item(0);
-    
+        print_r($node);
+        echo "<br>";
+        
         $textContent = $node->textContent;
+        print_r($textContent);
+        echo "<br>";
         $textContent = explode(");", explode("array(", explode(" = ", $textContent)[1])[1])[0];
+        print_r($textContent);
+        echo "<br>";
         $textContent = str_replace('"', "", $textContent);
+        print_r($textContent);
+        echo "<br>";
         $gradient = explode(",", $textContent);
+        print_r($gradient);
+        echo "<br>";
     
         $i = 0;
         $css = "<style>\r\n\r\n/*$steps*/\r\n\r\n";
