83d0a3fbe3975f0f7cf2f3003a14c03ceff812dc
adikul358 <adi.kul358@gmail.com>
2018-09-13 23:10:09 +0530

    test 6

--- php/functions.php
+++ php/functions.php
@@ -6,13 +6,14 @@
     // fetch color array from strangeplanet.fr
     function set_colors($steps) {
         $url="https://www.strangeplanet.fr/work/gradient-generator/?c=" . $steps . ":007E33:ff8800:C62828";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-        $html = curl_exec($ch);
-        curl_close($ch);
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        // $html = curl_exec($ch);
+        // curl_close($ch);
         // echo "<br>";
+        $html = file_get_contents($url);
         print_r("url: ");
         print_r($url);
         echo "<br>";
